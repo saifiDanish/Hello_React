77a24eee553a251a665b87f41c3c5b16a5fb8211
Mohd Danish Saifi <saifidanish549@gmail.com>
2023-08-24 12:15:07 +0530

    Episode 06 Added Shimmer and search functionality

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,37 +1,76 @@
 import RestaurantCard from "./RestaurantCard";
 import dataOBJ from "../utils/constant";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Shimmer } from "./Shimmer";
 
+const Body = () => {
+  const [resList, setresList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+  const[query,setQuery] = useState("");
 
-const Body = ()=>{
-    const [resList,setresList] = useState(dataOBJ);
-    return (
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-      <div className="body">
-  
-        {/* <div className="search">Search</div> */}
-        <button onClick={()=>{
-            // console.log({dataOBJ});
-            // dataOBJ.filter((data)=>{
-                //     return data.
-                // })
-                    const filterData = dataOBJ.filter((res)=>{
-                        // console.log();
-                        return res.info.avgRating>4.0;
-                    })
-                    setresList(filterData);
-                // console.log({filterData});
-        }}>Filter all top restaurant</button>
-        <div className="res-container"> 
-        
-        {resList.map((data)=>{
-          return <RestaurantCard resData={data}/>
-        })};
-        
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5691392&lng=77.2786314&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    setresList(
+      json?.data.cards[5]?.card?.card.gridElements?.infoWithStyle?.restaurants
+    );
+    setFilteredList(
+      json?.data.cards[5]?.card?.card.gridElements?.infoWithStyle?.restaurants
+    );
+    // console.log(json?.data.cards[5]?.card?.card.gridElements?.infoWithStyle?.restaurants);
+  };
+
+  if (resList.length === 0) return <Shimmer />;
+
+  return (
+    <div className="body">
+
+      <div className="filter-container">
+        <div className="search">
+          <input type="text" className="search-box" value={query} onChange={(e)=>{
+              setQuery(e.target.value);
+          }}/>
+          <button className="search-btn" onClick={()=>{
+            const filterdSearch = resList.filter((res)=>{
+              return res.info.name.toLowerCase().includes(query.toLocaleLowerCase());
+            })
+            // console.log(filterdSearch);
+            setFilteredList(filterdSearch);
+          }}>Search</button>
         </div>
-          
+        <button
+          onClick={() => {
+            const filterData = resList.filter((res) => {
+              return res.info.avgRating>4;
+            });
+            setFilteredList(filterData);
+          }}
+        className="filter-btn">
+          Filter all top restaurant
+        </button>
       </div>
-    )
-  }
 
-  export default Body;
+
+
+
+
+
+
+      <div className="res-container">
+        {filteredList.map((data) => {
+          return <RestaurantCard resData={data} />;
+        })}
+        ;
+      </div>
+    </div>
+  );
+};
+
+export default Body;
